4c03e129dd551327fb60923960c98ad41ddf5529
camo87 <duban5@gmail.com>
2022-08-18 15:30:22 -0500

    inicio

---
+++ src/main/java/listaciudades/listaciudades/ListaciudadesApplication.java
@@ -0,0 +1,13 @@
+package listaciudades.listaciudades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaciudadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaciudadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/listaciudades/listaciudades/ListaciudadesApplicationTests.java
@@ -0,0 +1,13 @@
+package listaciudades.listaciudades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaciudadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
